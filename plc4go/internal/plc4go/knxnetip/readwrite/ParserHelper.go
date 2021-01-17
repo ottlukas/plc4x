d125686cfe84a28bd69a7e9e2088ec1fa4416e46
Christofer Dutz <christofer.dutz@c-ware.de>
2021-01-17 12:35:48 +0100

    - Minor updates (Generally the order of imports so a reformat doesn't change the files too much)

--- plc4go/internal/plc4go/knxnetip/readwrite/ParserHelper.go
+++ plc4go/internal/plc4go/knxnetip/readwrite/ParserHelper.go
@@ -16,7 +16,7 @@
 // specific language governing permissions and limitations
 // under the License.
 //
-package readwrite
+package model
 
 import (
 	"github.com/apache/plc4x/plc4go/internal/plc4go/knxnetip/readwrite/model"
